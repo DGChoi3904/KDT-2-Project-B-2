636155c409be5000bbfbc129a20bef1123e80ef8
ujou173 <ujou173@gmail.com>
2023-06-26 15:28:56 +0900

    NestJS와 React 환경 세팅 완료

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'client', 'build'));
-  app.enableCors(); // 필요한 경우 CORS 설정
+
+  // 정적 파일 제공 설정
+  app.useStaticAssets(join(__dirname, '..', '..', 'client', 'build'));
+
+  // app.enableCors(); // 필요한 경우 CORS 설정
   await app.listen(5050);
 }
 bootstrap();
