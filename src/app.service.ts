d284d1f782f5714668d4a718ab316dc5215859d4
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-26 15:27:28 +0900

    탬플릿 TS을 적용하고, package의 옵션에 override 추가.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import path from 'path';
+import fs from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getPage(): void {
+    
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'client', 'build')); // 정적 파일을 읽어오는 경로 수정
+  app.useStaticAssets(join(__dirname, '..', '..', 'client', 'build')); // 정적 파일을 읽어오는 경로 수정
   app.enableCors(); // 필요한 경우 CORS 설정
   await app.listen(3000);
 }
