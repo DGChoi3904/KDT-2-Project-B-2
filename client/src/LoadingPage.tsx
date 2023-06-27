e2e157a37b9b04041dee8d7f5c0878818a707e8a
ujou173 <ujou173@gmail.com>
2023-06-27 17:35:09 +0900

    LoadingPage에 react-spinners-css 설치를 위한 주석 가이드 추가

--- client/src/LoadingPage.tsx
+++ client/src/LoadingPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// react-spinners-css 라는 npm module, 설치 필요 / client 디렉토리에서 -> npm install react-spinners-css
 import { Default } from 'react-spinners-css';
 // import logo from './logo.svg';
 import './App.css';
