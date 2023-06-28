0f86037b742514e159f993e52115478792546c87
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-28 11:16:40 +0900

    index.tsx에 SignUp 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-// import Loading from './LoadingPage';
-import MainPage from './MainPage/MainPage';
-// import TopMenu from './TopMenu';
-// import KakaoMap from './KakaoMap';
+import Loading from './LoadingPage';
+import TopMenu from './TopMenu';
+import KakaoMap from './KakaoMap';
+import SignUp from './SignUp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <MainPage />
-  </React.StrictMode>,
+    <TopMenu />
+    <KakaoMap />
+    <SignUp />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
