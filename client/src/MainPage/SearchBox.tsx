22e17e66120c9d7b9639198ab1384032047257c7
ujou173 <ujou173@gmail.com>
2023-06-28 15:17:28 +0900

    SearchBox 제작중 / 경유지 추가 버튼 기능 구현 완료

--- client/src/MainPage/SearchBox.tsx
+++ client/src/MainPage/SearchBox.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 
 const SearchBox: React.FC = () => {
+  const [wayPointCount, setWayPointCount] = useState(1);
+
+  const handleClick = () => {
+    setWayPointCount(prevCount => prevCount + 1);
+  };
+
   return(
     <div className='SearchBoxWrap'>
       <div className='FlexRowBetween'>
@@ -9,11 +15,16 @@ const SearchBox: React.FC = () => {
         <input type="text" className='SearchBar' />
         <div className='WayPointHiddenButton'></div>
       </div>
-      <div className='FlexRowBetween'>
+      {/* 경유지 설정 -> 초기 1개, + 버튼을 누를 때 마다 1개씩 추가 됨 */}
+      {Array.from({ length: wayPointCount }).map((_, index) => (
+        <div className='FlexRowBetween' key={index}>
         <p>경유지</p>
-        <input type="text" className='SearchBar' />
-        <button className='WayPointPlusButton'>+</button>
-      </div>
+          <input type="text" className='SearchBar' />
+          {index === wayPointCount - 1 ? (
+            <button className='WayPointPlusButton' onClick={handleClick}>+</button>
+          ) : <div className='WayPointHiddenButton'></div>}
+        </div>
+      ))}
       <div className='FlexRowBetween'>
         <p>목적지</p>
         <input type="text" className='SearchBar' />
