54c848b59010d0aae3aba1a9a18c8341d93c2acd
ParkDalJae <dv.over931@gmail.com>
2023-06-29 15:21:56 +0900

    github문서대로 세팅 + mongoDB 세팅

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 root/ 네스트 폴더  
 ├─┬client/ 리액트 폴더  
-│ ├─ build/  
-│ ├─ node_modeles/  
-│ ├─ public/  
-│ └─ src/  
+│ ├─build/  
+│ ├─node_modeles/  
+│ ├─public/  
+│ └─src/  
 ├──dist/  
 ├──node_modules/  
 ├──src/  
+├─┬─cats/  
+│ ├─┬─dto/  
+│ │ └──create-cat.dto.ts  
+│ ├─┬─schemas/  
+│'│ └──cats.schema.ts  
+│ └─┬─dto/  
+│'' ├──cats.controller.ts  
+│'' ├──cats.module.ts  
+│'' └──cats.service.ts  
 └──test/
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dvover931:1234@cluster0.p7kbkbj.mongodb.net/cats',
+    ),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
