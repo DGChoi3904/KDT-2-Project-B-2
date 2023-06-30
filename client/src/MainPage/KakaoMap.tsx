59cbc1f9b13724ef248a940f61177d7529f5a150
ujou173 <ujou173@gmail.com>
2023-06-30 16:31:42 +0900

    클릭 이벤트 global 변수를 사용하도록 변경 및 따로 이벤트 분리

--- client/src/MainPage/KakaoMap.tsx
+++ client/src/MainPage/KakaoMap.tsx
@@ -14,11 +14,11 @@ function KakaoMap() {
   const [places, setPlaces] = useState<Place[]>([]);
   const [selectedPlace, setSelectedPlace] = useState<Place | null>(null);
 
-  const [startPath, setStartPath] = useState<string[]>([]);
-  const [endPath, setEndPath] = useState<string[]>([]);
   const [roadPath, setRoadPath] = useState<number[]>([]);
   const [dataCheck, setDataCheck] = useState<boolean>(false);
+
   const mapRef = useRef<any>(null);
+
 
   // 지도 생성
   useEffect(() => {
@@ -31,133 +31,6 @@ function KakaoMap() {
     const map = new window.kakao.maps.Map(Container, Options);
     // map을 Ref값에 등록
     mapRef.current = map;
-    // 주소-좌표 변환 객체를 생성
-    let geocoder = new window.kakao.maps.services.Geocoder();
-
-    let startMarker = new window.kakao.maps.Marker(), // 출발지 위치를 표시할 마커.
-      startInfowindow = new window.kakao.maps.InfoWindow({ zindex: 1 }); // 출발지에 대한 주소를 표시할 인포윈도우
-    let endMarker = new window.kakao.maps.Marker(), // 목적지 위치를 표시할 마커.
-      endInfowindow = new window.kakao.maps.InfoWindow({ zindex: 6 }); // 목적지에 대한 주소를 표시할 인포윈도우
-
-    // 맵위의 클릭이벤트 리스너 파트.
-    window.kakao.maps.event.addListener(
-      map,
-      'click',
-      (mouseEvent: { latLng: any }) => {
-        // latLng를 사용하는  클릭 이벤트를 쓸 경우, 이 아래에 기입할 것.
-
-        // 전역변수 isSearchingStart가 True일 경우 실행되는 구문
-        if (globalVar.isSearchingStart) {
-          // 맵에서 출발지 마커를 찍어주는 함수 실행.
-          onClickSetStartPoint(mouseEvent);
-        } else if (globalVar.isSearchingEnd) {
-          onClickSetEndPoint(mouseEvent);
-        }
-      },
-    );
-    // 좌표로 상세 주소 정보를 요청하는 콜백함수
-    function searchDetailAddrFromCoords(
-      coords: { getLng: any; getLat: any },
-      callback: Function,
-    ) {
-      // geocoder를 통해 좌표로 상세 주소 정보를 요청
-      geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
-    }
-    // 맵을 클릭시 해당 좌표에 출발지 마커를 찍고 위치정보를 인포윈도우에 저장하는 함수
-    function onClickSetStartPoint(mouseEvent: { latLng: any }) {
-      searchDetailAddrFromCoords(
-        mouseEvent.latLng,
-        function (result: any, status: any) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            var detailAddr = !!result[0].road_address
-              ? '<div>도로명주소 : ' +
-                result[0].road_address.address_name +
-                '</div>'
-              : '';
-            detailAddr +=
-              '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
-
-            var content =
-              '<div style="padding:5px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">' +
-              '<span class="title">출발지 주소 정보</span>' +
-              detailAddr +
-              '</div>';
-
-            // 마커를 클릭한 위치에 표시
-            startMarker.setPosition(mouseEvent.latLng);
-            startMarker.setMap(map);
-
-            // 인포윈도우에 클릭한 위치에 대한 상세 주소정보를 표시
-            startInfowindow.setContent(content);
-            startInfowindow.open(map, startMarker);
-            globalVar.startPoint = [
-              mouseEvent.latLng.getLat(),
-              mouseEvent.latLng.getLng(),
-            ];
-            console.log(`출발지 좌표 : ${globalVar.startPoint}, 목적지 좌표 ${globalVar.endPoint}`);
-
-            // 출발지 지정 이후, 전역변수를 false로 설정.
-            globalVar.isSearchingStart = false;
-          }
-        },
-      );
-    }
-    // 맵을 클릭시 해당 좌표에 목적지 마커를 찍고 위치정보를 인포윈도우에 저장하는 함수
-    function onClickSetEndPoint(mouseEvent: { latLng: any }) {
-      searchDetailAddrFromCoords(
-        mouseEvent.latLng,
-        function (result: any, status: any) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            var detailAddr = !!result[0].road_address
-              ? '<div>도로명주소 : ' +
-                result[0].road_address.address_name +
-                '</div>'
-              : '';
-            detailAddr +=
-              '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
-
-            var content =
-              '<div style="padding:5px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">' +
-              '<span class="title">목적지 주소 정보</span>' +
-              detailAddr +
-              '</div>';
-
-            // 마커를 클릭한 위치에 표시
-            endMarker.setPosition(mouseEvent.latLng);
-            endMarker.setMap(map);
-
-            // 인포윈도우에 클릭한 위치에 대한 상세 주소정보를 표시
-            endInfowindow.setContent(content);
-            endInfowindow.open(map, endMarker);
-            globalVar.endPoint = [
-              mouseEvent.latLng.getLat(),
-              mouseEvent.latLng.getLng(),
-            ];
-            console.log(`출발지 좌표 : ${globalVar.startPoint}, 목적지 좌표 ${globalVar.endPoint}`);
-
-            // 목적지 지정 이후, 전역변수를 false로 설정.
-            globalVar.isSearchingEnd = false;
-          }
-        },
-      );
-    }
-
-    // 임시로 직접 클릭으로 좌표 지정 기능 살려 둠 //
-    window.kakao.maps.event.addListener(map, 'click', function (mouseEvent: { latLng: any }) {
-      const latlng = mouseEvent.latLng;
-
-      if (startPath.length === 0) {
-        setStartPath([latlng.getLat(), latlng.getLng()]);
-      } else {
-        setEndPath([latlng.getLat(), latlng.getLng()]);
-      }
-
-      const message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, 경도는 ' + latlng.getLng() + ' 입니다';
-      const resultDiv = document.getElementById('result')!;
-      resultDiv.innerHTML = message;
-      
-    });
-    // 임시로 직접 클릭으로 좌표 지정 기능 살려 둠 //
 
     const placesService = new window.kakao.maps.services.Places();
     const searchPlaces = (keyword: string) => {
@@ -210,14 +83,115 @@ function KakaoMap() {
     };
 
     addMarkersToMap();
+  }, []);
 
-  }, [startPath]);
+  // 클릭 이벤트 분리
+  useEffect(() => {
+    window.kakao.maps.event.addListener(mapRef.current, 'click', function (mouseEvent: { latLng: any }) {
+      let geocoder = new window.kakao.maps.services.Geocoder();
+  
+      let startMarker = new window.kakao.maps.Marker(), // 출발지 위치를 표시할 마커.
+        startInfowindow = new window.kakao.maps.InfoWindow({ zindex: 1 }); // 출발지에 대한 주소를 표시할 인포윈도우
+      let endMarker = new window.kakao.maps.Marker(), // 목적지 위치를 표시할 마커.
+        endInfowindow = new window.kakao.maps.InfoWindow({ zindex: 6 }); // 목적지에 대한 주소를 표시할 인포윈도우
 
-    // 확인용 console
-    useEffect(() => {
-      console.log('startPath: ', startPath)
-      console.log('endPath: ', endPath)
-    }, [startPath, endPath])
+      // 맵을 클릭시 해당 좌표에 출발지 마커를 찍고 위치정보를 인포윈도우에 저장하는 함수
+    function onClickSetStartPoint(mouseEvent: { latLng: any }) {
+      searchDetailAddrFromCoords(
+        mouseEvent.latLng,
+        function (result: any, status: any) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            var detailAddr = !!result[0].road_address
+              ? '<div>도로명주소 : ' +
+                result[0].road_address.address_name +
+                '</div>'
+              : '';
+            detailAddr +=
+              '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+
+            var content =
+              '<div style="padding:5px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">' +
+              '<span class="title">출발지 주소 정보</span>' +
+              detailAddr +
+              '</div>';
+
+            // 마커를 클릭한 위치에 표시
+            startMarker.setPosition(mouseEvent.latLng);
+            startMarker.setMap(mapRef.current);
+
+            // 인포윈도우에 클릭한 위치에 대한 상세 주소정보를 표시
+            startInfowindow.setContent(content);
+            startInfowindow.open(mapRef.current, startMarker);
+            globalVar.startPoint = [
+              mouseEvent.latLng.getLat(),
+              mouseEvent.latLng.getLng(),
+            ];
+            console.log(`출발지 좌표 : ${globalVar.startPoint}, 목적지 좌표 ${globalVar.endPoint}`);
+
+            // 출발지 지정 이후, 전역변수를 false로 설정.
+            globalVar.isSearchingStart = false;
+          }
+        },
+      );
+    };
+
+    // 맵을 클릭시 해당 좌표에 목적지 마커를 찍고 위치정보를 인포윈도우에 저장하는 함수
+    function onClickSetEndPoint(mouseEvent: { latLng: any }) {
+      searchDetailAddrFromCoords(
+        mouseEvent.latLng,
+        function (result: any, status: any) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            var detailAddr = !!result[0].road_address
+              ? '<div>도로명주소 : ' +
+                result[0].road_address.address_name +
+                '</div>'
+              : '';
+            detailAddr +=
+              '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+
+            var content =
+              '<div style="padding:5px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">' +
+              '<span class="title">목적지 주소 정보</span>' +
+              detailAddr +
+              '</div>';
+
+            // 마커를 클릭한 위치에 표시
+            endMarker.setPosition(mouseEvent.latLng);
+            endMarker.setMap(mapRef.current);
+
+            // 인포윈도우에 클릭한 위치에 대한 상세 주소정보를 표시
+            endInfowindow.setContent(content);
+            endInfowindow.open(mapRef.current, endMarker);
+            globalVar.endPoint = [
+              mouseEvent.latLng.getLat(),
+              mouseEvent.latLng.getLng(),
+            ];
+            console.log(`출발지 좌표 : ${globalVar.startPoint}, 목적지 좌표 ${globalVar.endPoint}`);
+
+            // 목적지 지정 이후, 전역변수를 false로 설정.
+            globalVar.isSearchingEnd = false;
+          }
+        },
+      );
+    }
+
+    // 좌표로 상세 주소 정보를 요청하는 콜백함수
+    function searchDetailAddrFromCoords(
+      coords: { getLng: any; getLat: any },
+      callback: Function,
+    ) {
+      // geocoder를 통해 좌표로 상세 주소 정보를 요청
+      geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    }
+
+      if (globalVar.isSearchingStart) {
+        // 맵에서 출발지 마커를 찍어주는 함수 실행.
+        onClickSetStartPoint(mouseEvent);
+      } else if (globalVar.isSearchingEnd) {
+        onClickSetEndPoint(mouseEvent);
+      }
+    });
+  }, [])
 
   // polyline 그리기
   useEffect(() => {
@@ -247,48 +221,9 @@ function KakaoMap() {
     }
   }, [dataCheck, roadPath]);
 
-  // ! removeListener 해결 전까지 임시로 예전 코드 복구해두고 기능 막아둠
-  // const startPoint = () => window.kakao.maps.event.addListener(mapRef.current, 'click', function (mouseEvent: { latLng: any }) {
-  //   const latlng = mouseEvent.latLng;
-  //   console.log('handleStart')
-  //   setStartPath([latlng.getLat(), latlng.getLng()])
-  //   const message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, 경도는 ' + latlng.getLng() + ' 입니다';
-  //   const resultDiv = document.getElementById('result')!;
-  //   resultDiv.innerHTML = message;
-  // });
-
-  // const endPoint = () => window.kakao.maps.event.addListener(mapRef.current, 'click', function (mouseEvent: { latLng: any }) {
-  //   const latlng = mouseEvent.latLng;
-  //   console.log('handleEnd')
-  //   setEndPath([latlng.getLat(), latlng.getLng()])
-  //   const message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, 경도는 ' + latlng.getLng() + ' 입니다';
-  //   const resultDiv = document.getElementById('result')!;
-  //   resultDiv.innerHTML = message;
-  // });
-
-  // const handleNaviStart = () => {
-  //   window.kakao.maps.event.removeListener(mapRef.current, 'click', endPoint());
-  //   if (mapRef.current) {
-  //     console.log('handleNaviStart')
-  //     startPoint();
-  //   }
-  //   // removeListener() 를 통해 addListener()를 제거해야 함
-  //   // removeListener()를 사용하려면 const aaa = window.kakao.maps.event.addListener(mapRef.current, 'click', function (mouseEvent: { latLng: any }) { ... } 이런 형식으로 사용해야함
-  //   // 지금처럼 바로 addListener()를 사용하면 참조값을 찾을 수 없어 해제가 불가능
-  // }
-
-  // const handleNaviEnd = () => {
-  //   window.kakao.maps.event.removeListener(mapRef.current, 'click', startPoint());
-  //   if (mapRef.current) {
-  //     console.log('handleNaviEnd')
-  //     endPoint();
-  //   }
-  //   // removeListener() 를 통해 addListener()를 제거해야 함
-  // }
-
   // 경로안내 버튼 클릭 시 지정된 출발지/도착지 정보를 가지고 최단거리 산출
   const handleNavi = () => {
-    const url = `https://apis-navi.kakaomobility.com/v1/directions?priority=RECOMMEND&car_type=1&car_fuel=GASOLINE&origin=${startPath[1]}%2C+${startPath[0]}&destination=${endPath[1]}%2C+${endPath[0]}`;
+    const url = `https://apis-navi.kakaomobility.com/v1/directions?priority=RECOMMEND&car_type=1&car_fuel=GASOLINE&origin=${globalVar.startPoint[1]}%2C+${globalVar.startPoint[0]}&destination=${globalVar.endPoint[1]}%2C+${globalVar.endPoint[0]}`;
     console.log('url: ', url);
 
     const headers = {
