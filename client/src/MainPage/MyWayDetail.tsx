61dc9165a0498c8e05cb812644d4e8ec2095f1b3
DGChoi3904 <dgchoi3904@gmail.com>
2023-07-03 16:42:48 +0900

    버튼 스타일 적용, 소요시간 시/분/초로 정제 및 세팅하기, 설명 주석 추가.

--- client/src/MainPage/MyWayDetail.tsx
+++ client/src/MainPage/MyWayDetail.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 
 // 페이지 작업용 검색 결과 json 값 추가.
 interface wayInfo {
-  name : string,
-  distance : number,
-  state : string,
-  stateText : string,
-  duration : number,
-
+  name: string;
+  distance: number;
+  state: string;
+  stateText: string;
+  duration: string;
 }
 
-
 const MyWayDetail: React.FC = () => {
-
   const wayResult = {
     trans_id: '0188fbec0082724392ac52fe9e946e49',
     routes: [
@@ -186,15 +183,21 @@ const MyWayDetail: React.FC = () => {
       },
     ],
   };
-  const wayInfo : any[] = wayResult.routes[0].sections[0].roads
-    .map((wayNames) => {
+  const MyWayName = {
+    index : 1,
+    name : "저장된 MyWay 경로 명"
+  }
+  // fetch로 받아온 길찾기 API 결과값에서 roads에서 필요한 데이터만 꺼내 저장하는 구문.
+  // 만약 fetch로 요청 결과 JSON을 상속받았을 경우, wayResult에 할당하여 적용하면 된다.
+  const wayInfo: any[] = wayResult.routes[0].sections[0].roads.map(
+    (wayNames) => {
       let roadName, roadDistance, roadState, roadStateText, roadDuration;
-      if(wayNames.name === ""){
-        roadName = "도로";
+      if (wayNames.name === '') {
+        roadName = '도로';
       } else {
         roadName = wayNames.name;
       }
-      switch(wayNames.traffic_state) {
+      switch (wayNames.traffic_state) {
         case 0:
           roadState = '#2DB400';
           roadStateText = '정보없음';
@@ -226,28 +229,97 @@ const MyWayDetail: React.FC = () => {
       }
       roadDistance = wayNames.distance;
       
-      roadDuration = wayNames.duration;
-      return {name : roadName, distance: roadDistance, state : roadState, stateText : roadStateText, duration: roadDuration};
-    });
+      //
+      const timeSum: number = wayNames.duration;
+      let hour : number = 0;
+      let minutes :number = 0;
+      let seconds : number = 0;
+      // 분 계산
+      if (Math.floor(timeSum / 60) > 60) {
+        hour = Math.floor(Math.floor(timeSum / 60) / 60);
+        minutes = Math.floor(timeSum / 60) % 60;
+      } else {
+        minutes = Math.floor(timeSum / 60);
+      }
+      // 초 계산
+      seconds = timeSum % 60;
+      roadDuration = (hour !== 0 ? `${hour}시간`:'') + " " + (minutes !== 0 ? `${minutes}분` : '') + " " + (seconds !== 0 ? `${seconds}초`: '');
+      return {
+        name: roadName,
+        distance: roadDistance,
+        state: roadState,
+        stateText: roadStateText,
+        duration: roadDuration,
+      };
+    },
+  );
+
   return (
     <div>
       <div className="MyWayListTitle">
-        <p>#1 저장된 MyWay 경로 명</p>
-        <div>뒤로 가기</div>
+        <p>#{MyWayName.index} {MyWayName.name}</p>
+        <button type="button" className="MyWayDetail-SaveButton">
+          저장하기
+        </button>
       </div>
       <div style={{ height: '195px', backgroundColor: 'beige' }}>
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', margin: '2px'}}>
-          <p style={{width: '10%', textAlign: 'center', fontWeight:'bolder'}}>#</p>
-            <p style={{width: '50%', textAlign: 'center',  fontWeight:'bolder'}}>길 이름</p>
-            <p style={{width: '20%', textAlign: 'center', fontWeight:'bolder'}}>교통 상황</p>
-            <p style={{width: '20%', textAlign: 'center', fontWeight:'bolder'}}>소요 시간</p>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '2px',
+          }}
+        >
+          <p
+            style={{ width: '10%', textAlign: 'center', fontWeight: 'bolder' }}
+          >
+            #
+          </p>
+          <p
+            style={{ width: '40%', textAlign: 'center', fontWeight: 'bolder' }}
+          >
+            길 이름
+          </p>
+          <p
+            style={{ width: '20%', textAlign: 'center', fontWeight: 'bolder' }}
+          >
+            교통 상황
+          </p>
+          <p
+            style={{ width: '30%', textAlign: 'center', fontWeight: 'bolder' }}
+          >
+            소요 시간
+          </p>
         </div>
-        {wayInfo.map((way : wayInfo, index : number) => (
-          <div key={index} style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-            <p style={{width: '10%', paddingLeft:'10px'}}>#{index + 1}</p>
-            <p style={{width: '50%'}}>{way.name}</p>
-            <p style={{width: '20%', color : way.state, fontWeight: 'bolder', textAlign: 'center', textShadow: '1px 1px #000'}}>{way.stateText}</p>
-            <p style={{width: '20%', textAlign: 'right', paddingRight: '10px'}}>{way.duration}초</p>
+        {wayInfo.map((way: wayInfo, index: number) => (
+          <div
+            key={index}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <p style={{ width: '10%', paddingLeft: '10px' }}>#{index + 1}</p>
+            <p style={{ width: '40%' }}>{way.name}</p>
+            <p
+              style={{
+                width: '20%',
+                color: way.state,
+                fontWeight: 'bolder',
+                textAlign: 'center',
+                textShadow: '1px 1px #000',
+              }}
+            >
+              {way.stateText}
+            </p>
+            <p
+              style={{ width: '30%', textAlign: 'right', paddingRight: '10px' }}
+            >
+              {way.duration}</p>
           </div>
         ))}
       </div>
