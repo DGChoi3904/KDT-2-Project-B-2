76f0f341fb31ea9c2e8d2fe4c7c2e6123b859f87
DGChoi3904 <dgchoi3904@gmail.com>
2023-07-04 14:42:59 +0900

    DB에서 데이터를 받고, MyWayList에서 리스트를 생성, Props로 데이터 전달. Fixes #31

--- client/src/MainPage/MyWayList.tsx
+++ client/src/MainPage/MyWayList.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
+import KakaoMap from './KakaoMap';
 import './Main.css';
 
 import MyWayComponent from './MyWayComponent';
 
 function MyWayList() {
+  const userDbSample = [];
   const count = 3;
 
   return (
     <div>
-      <div className='MyWayListTitle'>
-        <p>
-          MyWay 목록
-        </p>
-        <div>
-          UI 숨기기
-        </div>
+      <div className="MyWayListTitle">
+        <p>MyWay 목록</p>
+        <div>UI 숨기기</div>
       </div>
-      <div style={{height: '195px', backgroundColor: 'beige'}}>
-      {Array.from({ length: count }).map((_, index) => (
+      <div style={{ height: '195px', backgroundColor: 'beige' }}>
+        {Array.from({ length: count }).map((_, index) => (
           <MyWayComponent key={index} index={index + 1} />
         ))}
       </div>
     </div>
-  )
+  );
 }
-
 
 export default MyWayList;
