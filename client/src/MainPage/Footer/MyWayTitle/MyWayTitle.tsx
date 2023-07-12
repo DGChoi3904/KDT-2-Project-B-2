851cfab3f98ab3c3738795c8e8b87489298b0f64
DGChoi3904 <dgchoi3904@gmail.com>
2023-07-12 15:15:42 +0900

    MyWayTitle에서 해당 Props가 바뀔때마다 출력되는지 useEffect로 테스트

--- client/src/MainPage/Footer/MyWayTitle/MyWayTitle.tsx
+++ client/src/MainPage/Footer/MyWayTitle/MyWayTitle.tsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import '../MyWayCSS.css'
+import React, { useEffect, useState } from 'react';
+import '../MyWayCSS.css';
 
 type MyWayTitleProps = {
   myWayUI: boolean;
   setMyWayUI: React.Dispatch<React.SetStateAction<boolean>>;
-}
+  currentMyWayNameObj: {
+    index: number;
+    name: string;
+  };
+};
 
-const MyWayTitle: React.FC<MyWayTitleProps> = ({ myWayUI, setMyWayUI }) => {
-
+const MyWayTitle: React.FC<MyWayTitleProps> = ({
+  myWayUI,
+  setMyWayUI,
+  currentMyWayNameObj,
+}) => {
   const handleUIHidden = () => {
-    setMyWayUI(false)
-  }
+    setMyWayUI(false);
+  };
 
   const handleUIShow = () => {
-    setMyWayUI(true)
-  }
+    setMyWayUI(true);
+  };
+  useEffect(() => {
+    console.log(currentMyWayNameObj);
+  }, [currentMyWayNameObj.index]);
 
   return (
     <div className="MyWayListTitle">
-        <p>MyWay 목록</p>
-        {myWayUI ? <button onClick={handleUIHidden}>UI 숨기기</button> : <button onClick={handleUIShow}>UI 보이기</button>}
+      <p>MyWay 목록</p>
+      {myWayUI ? (
+        <button onClick={handleUIHidden}>UI 숨기기</button>
+      ) : (
+        <button onClick={handleUIShow}>UI 보이기</button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MyWayTitle
+export default MyWayTitle;
