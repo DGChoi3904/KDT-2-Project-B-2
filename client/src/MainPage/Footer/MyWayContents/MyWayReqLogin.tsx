cffae9340b9d4b9831c3c52321d01d139a9611ed
DGChoi3904 <dgchoi3904@gmail.com>
2023-07-12 12:49:03 +0900

    ReqLogin에서 이전한 스타일을 클래스로 적용

--- client/src/MainPage/Footer/MyWayContents/MyWayReqLogin.tsx
+++ client/src/MainPage/Footer/MyWayContents/MyWayReqLogin.tsx
@@ -4,20 +4,11 @@ import '../MyWayCSS.css';
 const MyWayReqLogin = () => {
   return (
     <div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '195px',
-          backgroundColor: 'beige',
-        }}
-      >
+      <div className="flex-row justify-center align-center myway-contentsbox">
         로그인 필요
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MyWayReqLogin;
