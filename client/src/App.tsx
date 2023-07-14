54ecf9b23180752fe8a3791f5801372ba4848ca4
ujou173 <ujou173@gmail.com>
2023-07-14 12:52:25 +0900

    env를 이용한 은닉화 실패

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,20 @@ interface MyWayNameObj {
 }
 
 function App() {
+  const [apiLoaded, setApiLoaded] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_API_KEY}&libraries=services`;
+    script.async = true;
+    script.onload = () => {
+      setTimeout(() => {setApiLoaded(true)}, 1500);
+      // setApiLoaded(true);
+    };
+    document.body.appendChild(script);
+    console.log(process.env.REACT_APP_API_KEY);
+  }, []);
+
   const [startPoint, setStartPoint] = useState<[number, number]>([0, 0]);
   const [endPoint, setEndPoint] = useState<[number, number]>([0, 0]);
   const [wayPoint, setWayPoint] = useState<number[]>([]);
@@ -34,32 +48,32 @@ function App() {
   })
 
   useEffect(() => {
-    console.log('로그인 상태 체크: ', loginCheck)
-    console.log('로그인 상태 체크22: ', localStorage.getItem('loginCheck'))
-  }, [])
-
-  useEffect(() => {
     localStorage.setItem('loginCheck', JSON.stringify(loginCheck));
     console.log('login체커: ', loginCheck);
   }, [loginCheck]);
 
-  return (
-    <div>
-      <MapContext.Provider value={{ startPoint, setStartPoint, endPoint, setEndPoint, wayPoint, setWayPoint, isSearchingStart, setIsSearchingStart, isSearchingEnd, setIsSearchingEnd }}>
-        <LoginContextProvider>
-          <NaviProvider>
-          <MyWayContext.Provider value={{myWayUI, setMyWayUI, detail, setDetail, currentMyWayNameObj, setCurrentMyWayNameObj}}>
-            <Routes>
-              <Route path="/" element={<Loading />} />
-              <Route path="/main" element={<MainPage />} />
-              <Route path="/signup" element={<SignUp />} />
-            </Routes>
-          </MyWayContext.Provider>
-          </NaviProvider>
-        </LoginContextProvider>
-      </MapContext.Provider>
-    </div>
-  )
+  if (!apiLoaded) {
+    // API 스크립트가 로드되지 않은 경우 로딩 표시 등을 표시할 수 있습니다.
+    return <div><Loading /></div>;
+  } else {
+    return (
+      <div>
+        <MapContext.Provider value={{ startPoint, setStartPoint, endPoint, setEndPoint, wayPoint, setWayPoint, isSearchingStart, setIsSearchingStart, isSearchingEnd, setIsSearchingEnd }}>
+          <LoginContextProvider>
+            <NaviProvider>
+            <MyWayContext.Provider value={{myWayUI, setMyWayUI, detail, setDetail, currentMyWayNameObj, setCurrentMyWayNameObj}}>
+              <Routes>
+                <Route path="/" element={<Loading />} />
+                <Route path="/main" element={<MainPage />} />
+                <Route path="/signup" element={<SignUp />} />
+              </Routes>
+            </MyWayContext.Provider>
+            </NaviProvider>
+          </LoginContextProvider>
+        </MapContext.Provider>
+      </div>
+    )
+  }
 }
 
 export default App;
